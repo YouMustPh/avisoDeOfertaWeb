3aaed0868eb740ab6fae0e61bdc22e2569f5f133
YouMustPh <phps6200@gmail.com>
2024-02-05 15:22:47 -0300

    feat: speed insights

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,6 +3,7 @@
 import { Toaster } from "@/components/ui/toaster";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import * as React from "react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export function Providers(props: { children: React.ReactNode }) {
   const queryClient = new QueryClient();
@@ -10,6 +11,7 @@ export function Providers(props: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       <Toaster />
+      <SpeedInsights />
       {props.children}
     </QueryClientProvider>
   );
